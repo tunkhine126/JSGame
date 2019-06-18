d021d02c20e499b1acf72c382017e7b261b44e52
tunkhine126 <tunkhine126@gmail.com>
2019-06-18 10:56:48 -0400

    TwoModels

---
+++ backend/app/serializers/topic_serializer.rb
@@ -0,0 +1,5 @@
+class TopicSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :user_id
+end
+
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :user
+resources :topics, only: [:index, :show]
+resources :questions, only: [:index, :show]
+resources :answers, only: [:index, :show]
 end
