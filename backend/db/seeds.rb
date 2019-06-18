8641bb5faa9f30d9eb4a7e90bb8f88ab8b949ecb
tunkhine126 <tunkhine126@gmail.com>
2019-06-18 11:10:29 -0400

    updated

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -22,7 +22,12 @@ question2 = Question.create(name: 'What year was Jacascript created?', topic_id:
 question3 = Question.create(name: 'What is a variable?', topic_id: topic2.id)
 question4 = Question.create(name: 'Do we still us var as a variable declaration in 2019?', topic_id: topic2.id)
 
-answer1 = Answer.create(name: 'Object-oriented computer programming language', question_id: question1.id)
+answer1 = Answer.create(name: 'Object-oriented computer programming language', question_id: question1.id, correct: true)
+answer5 = Answer.create(name: 'THIS', question_id: question1.id, correct: false)
+answer6 = Answer.create(name: 'IS', question_id: question1.id, correct: false)
+answer7 = Answer.create(name: 'WRONG', question_id: question1.id, correct: false)
+
+
 answer2 = Answer.create(name: '1995', question_id: question2.id)
 answer3 = Answer.create(name: 'A container of values that can be changed at anytime.', question_id: question3.id)
 answer4 = Answer.create(name: 'NO', question_id: question4.id)
